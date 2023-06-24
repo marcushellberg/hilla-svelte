9b56a2188b0fdda6b35527c6ad0045b803adb7ef
Marcus Hellberg <marcus@vaadin.com>
2023-06-24 16:08:26 +0200

    remove debug log

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-console.log('************************HERE')
-
 export default {
     // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
     // for more information about preprocessors
